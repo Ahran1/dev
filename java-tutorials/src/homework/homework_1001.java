e32efbddfaba0eb64ae5204e353ba42c312249c7
shin <miahone@naver.com>
2021-04-01 20:01:37 +0900

    homework 1001 ~ 1008

---
+++ java-tutorials/src/homework/homework_1001.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class homework_1001 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+		
+	}
+
+}
---
+++ java-tutorials/src/homework/homework_1007.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class homework_1007 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+		
+	}
+
+}
