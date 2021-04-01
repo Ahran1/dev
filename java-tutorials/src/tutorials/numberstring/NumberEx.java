e533f9b01508d419be70fea04221195d0916b486
shin <miahone@naver.com>
2021-04-01 19:41:42 +0900

    lecture 0330

---
+++ java-tutorials/src/tutorials/numberstring/NumberEx.java
@@ -0,0 +1,10 @@
+package tutorials.numberstring;
+
+public class NumberEx {
+	public static void main(String[] args) {
+		System.out.println(1 + 2);
+		System.out.println(1.2 + 1.3);
+		System.out.println(2 * 5);
+		System.out.println(6 / 2);
+	}
+}
---
+++ java-tutorials/src/tutorials/numberstring/StringEx.java
@@ -0,0 +1,20 @@
+package tutorials.numberstring;
+
+public class StringEx {
+	public static void main(String[] args) {
+		System.out.println('A');
+		System.out.println("ABCD");
+//		System.out.println('ABCD');
+		System.out.println("A");
+		
+		System.out.println("A" + "BCD");
+		System.out.println(1 + 1);
+		System.out.println("1" + "1");
+		
+//		System.out.println("I said, "Hello world"");
+		System.out.println("I said, \"Hello world\"");
+		System.out.println("I said, \nHello world");
+		System.out.println("I said, \tHello world");
+		
+	}
+}
