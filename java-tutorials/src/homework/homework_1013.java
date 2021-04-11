f1a070efc35318b8bc681b05046a85024e4fe4dd
shin <miahone@naver.com>
2021-04-11 23:44:11 +0900

    homework_1010

---
+++ java-tutorials/src/homework/homework_1013.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class homework_1013 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\u250C\u252C\u2510");
+        System.out.println("\u251C\u253C\u2524");
+        System.out.println("\u2514\u2534\u2518");
+		
+	}
+
+}
---
+++ java-tutorials/src/homework/homework_1018.java
@@ -0,0 +1,19 @@
+package homework;
+
+import java.util.Scanner;
+
+public class homework_1018 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		scanner.useDelimiter("[:\n]");
+		int h = scanner.nextInt();
+		int m = scanner.nextInt();
+		System.out.println(h);
+		System.out.println(m);
+		scanner.close();
+		
+	}
+
+}
