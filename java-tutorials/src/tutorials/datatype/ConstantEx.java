52cdc5170137d56acbc740dd216275a716211eb4
shin <miahone@naver.com>
2021-04-18 23:55:59 +0900

    4/15수업

---
+++ java-tutorials/src/tutorials/datatype/ConstantEx.java
@@ -0,0 +1,22 @@
+package tutorials.datatype;
+
+public class ConstantEx {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+//		1 = 3;
+		
+//		int b = 1;
+//		int c = 2.2;
+		
+		long d = 2147483648L;
+		
+//		float f = 2.2;
+		
+//		byte e = 100;
+//		short f = 200;
+		
+	}
+
+}
---
+++ java-tutorials/src/tutorials/typeconversion/TypeConversionEx.java
@@ -0,0 +1,25 @@
+package tutorials.typeconversion;
+
+public class TypeConversionEx {
+
+	public static void main(String[] args) {
+
+//		int a = 200.0;
+//		double a = 3.0;
+//		float a = 3.0;
+//		int a = 200;
+//		long a = 200;
+//		double a = 3.0f;
+
+		int a = 3;
+		float b = 1.0F;
+		double c = a + b;
+		
+		// explicit type conversion
+//		float a = (float)100.0;
+//		int b = 100.0F;
+//		long c = 100L;
+//		int a = c;
+	}
+
+}
