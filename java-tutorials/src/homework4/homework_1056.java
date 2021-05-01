f59ef639b9217df1cf6b8e8fa62061339c1bef80
shin <miahone@naver.com>
2021-05-01 16:33:23 +0900

    1056 수정

--- java-tutorials/src/homework4/homework_1056.java
+++ java-tutorials/src/homework4/homework_1056.java
@@ -7,12 +7,12 @@ public class homework_1056 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		
-		System.out.printf("%d", a != 0 ^ b != 0 ? 1 : 0);
-		
+
+		boolean a = scanner.nextInt() != 0;
+		boolean b = scanner.nextInt() != 0;
+
+		System.out.printf("%d", a && !b || !a && b ? 1 : 0);
+
 		scanner.close();
 	}
 
