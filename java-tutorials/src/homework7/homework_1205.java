0bd4eec5f27e929d31fc41486ad39d91758f2aee
shin <miahone@naver.com>
2021-05-10 20:50:25 +0900

    1205수정

--- java-tutorials/src/homework7/homework_1205.java
+++ java-tutorials/src/homework7/homework_1205.java
@@ -7,27 +7,33 @@ public class homework_1205 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
 		scanner.close();
-		double c = a + b;
-		double d = Math.max(a - b, b - a);
-		double e = a * b;
-		double f = Math.max(a / b, b / a);
-		double g = Math.max(Math.pow((double) a, (double) b), Math.pow((double) b, (double) a));
-		double max = 0;
-		if (c >= d && c >= e && c >= f && c >= g) {
-			max = c;
-		} else if (d >= e && d >= f && d >= g && d >= c) {
-			max = d;
-		} else if (e >= f && e >= g && e >= c && e >= d) {
-			max = e;
-		} else if (f >= g && f >= c && f >= d && f >= e) {
-			max = f;
-		} else if (g >= c && g >= d && g >= e && g >= f) {
-			max = g;
+
+		double max = a + b;
+		if (a - b > max) {
+			max = a - b;
 		}
-		System.out.printf("%.6f", (double) max);
+		if (b - a > max) {
+			max = b - a;
+		}
+		if (a * b > max) {
+			max = a * b;
+		}
+		if (a / b > max) {
+			max = a / b;
+		}
+		if (b / a > max) {
+			max = b / a;
+		}
+		if (Math.pow(a, b) > max) {
+			max = Math.pow(a, b);
+		}
+		if (Math.pow(b, a) > max) {
+			max = Math.pow(b, a);
+		}
+		System.out.printf("%.6f", max);
 
 	}
 }
