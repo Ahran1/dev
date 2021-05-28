c01c4dc66b78984ebcae8f99014135f2242c0ee1
Your Name <you@example.com>
2021-05-28 17:54:20 +0900

    이상한거삭제..

--- java-tutorials/src/homework11/Homework_1354.java
+++ java-tutorials/src/homework11/Homework_1354.java
@@ -2,43 +2,20 @@ package homework11;
 
 import java.util.Scanner;
 
-public class Homework_1357 {
+public class Homework_1354 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		scanner.close();
-		
-		
-		
+
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
 	}
 
 }
-/*
-
-`		i1j1
-``		i2j1 i2j2
-```		i3j1 i3j2 i3j3
-````	i4j1 i4j2 i4j3 i4j4
-```		i5j1 i5j2 i5j3
-``		i6j1 i6j2
-`		i7j1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
