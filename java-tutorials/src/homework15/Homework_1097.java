9642a18aca153e5fcc650b71fe2038049bac97d5
shin <miahone@naver.com>
2021-06-28 22:24:32 +0900

    수업시간문제푼것

--- java-tutorials/src/homework15/Homework_1097.java
+++ java-tutorials/src/homework15/Homework_1097.java
@@ -5,46 +5,35 @@ import java.util.Scanner;
 public class Homework_1097 {
 
 	public static void main(String[] args) {
+		int[][] board = new int[20][20];
+		int n, x, y;
+
 		Scanner scanner = new Scanner(System.in);
-
-		int[][] arr = new int[20][20];
 		for (int i = 1; i < 20; i++) {
 			for (int j = 1; j < 20; j++) {
-				arr[i][j] = scanner.nextInt();
+				board[i][j] = scanner.nextInt();
 			}
 		}
 
-		int n = scanner.nextInt();
-
+		n = scanner.nextInt();
 		for (int i = 0; i < n; i++) {
-			int x = scanner.nextInt();
-			int y = scanner.nextInt();
+			y = scanner.nextInt();
+			x = scanner.nextInt();
 			for (int j = 1; j < 20; j++) {
-				for (int k = 1; k < 20; k++) {
-					if(arr[j][y]==0) {
-						arr[j][y] = 1;
-					} else {
-						arr[j][y] = 0;
-					}
-					if(arr[x][k]==0) {
-						arr[x][k] = 1;
-					} else {
-						arr[x][k] = 0;
-					}
-									
-				
-				}
+				board[y][j] = 1 - board[y][j];
+			}
+
+			for (int j = 1; j < 20; j++) {
+				board[j][x] = 1 - board[j][x];
 			}
 		}
 
 		for (int i = 1; i < 20; i++) {
 			for (int j = 1; j < 20; j++) {
-				System.out.print(arr[i][j] + " ");
+				System.out.printf("%d ", board[i][j]);
 			}
 			System.out.println();
 		}
-
 		scanner.close();
 	}
-
 }
--- java-tutorials/src/homework16/Homework_1098.java
+++ java-tutorials/src/homework16/Homework_1098.java
@@ -7,36 +7,36 @@ public class Homework_1098 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		int w = scanner.nextInt();
-		int h = scanner.nextInt();
-		int n = scanner.nextInt();
-		int l, d, x, y;
-		int[][] arr = new int[101][101];
+		int[][] a = new int[10][10];
+		int[] dx = { 1, 0 };
+		int[] dy = { 0, 1 };
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				a[i][j] = scanner.nextInt();
+			}
+		}
+		scanner.close();
 
-		for (int i = 0; i < n; i++) {
-			l = scanner.nextInt();
-			d = scanner.nextInt();
-			x = scanner.nextInt();
-			y = scanner.nextInt();
-
-			if (d == 0) {
-				for (int j = 0; j < l; j++) {
-					arr[x][y + j] = 1;
-				}
-			} else {
-				for (int j = 0; j < l; j++) {
-					arr[x + j][y] = 1;
+		int x = 1, y = 1, nx = 1, ny = 1;
+		while (a[y][x] != 2 && x == nx && y == ny) {
+			a[y][x] = 9;
+			for (int i = 0; i < 2; i++) {
+				nx = x + dx[i];
+				ny = y + dy[i];
+				if (a[ny][nx] != 1) {
+					x = nx;
+					y = ny;
+					break;
 				}
 			}
 		}
+		a[y][x] = 9;
 
-		for (int i = 1; i <= w; i++) {
-			for (int j = 1; j <= h; j++) {
-				System.out.print(arr[i][j] + " ");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.printf("%d ", a[i][j]);
 			}
 			System.out.println();
 		}
-
 	}
-
 }
